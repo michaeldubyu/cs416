b5fcbfde381cd65fe8639cf9ec5ae6428b9bc9a5
michaeldubyu <michaeldubyu@gmail.com>
2014-03-23 23:17:00 -0700

    real random ordering

--- assign3/electleader.c
+++ assign3/electleader.c
@@ -56,7 +56,7 @@ int main(int argc, char** argv){
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank ); 
     MPI_Comm_size( MPI_COMM_WORLD, &size );
-    pid = (rank + 1) * PNUM  % size;
+    pid = ((rank + 1) * PNUM)  % size;
 
     beginElection();
 
